a39c81eb2382a8b05cecd7b28f235f4bdb2e96e7
Mohamed Abdel Maksoud <mohamed@amaksoud.com>
2019-12-22 15:23:48 +0100

    moves lists functionality to a separate module

--- clodss.py
+++ clodss.py
@@ -1,106 +1,27 @@
 import os
 from router import Router
+import lists
+
 
 class StrictRedis:
-    def __init__(self, db: int = 0, sharding_factor: int = 3, base: str = 'data', decode_responses: bool = False) -> None:
+    def __init__(
+            self, db: int = 0, sharding_factor: int = 3, base: str = 'data',
+            decode_responses: bool = False) -> None:
         self.decode = decode_responses
         dbpath = os.path.join(os.getcwd(), base, '%02d' % db)
         os.makedirs(dbpath, exist_ok=True)
         self.router = Router(dbpath, sharding_factor)
         self._knownkeys = set()
 
-    # TODO IOC dtype to handlers
-
-    def _ensure_exists(self, db, key, dtype):
-        if key in self._knownkeys:
-            return
-        TABLES = {
-            'list': ['<key>-l', '<key>-r']
-        }
-
-        tables = [tname.replace('<key>', key) for tname in TABLES[dtype]]
-        x = db.execute('SELECT 1 FROM sqlite_master WHERE '
-                       'type="table" AND name=?', (tables[0],)).fetchone()
-        if x is None:
-            for table in tables:
-                db.execute(f'CREATE TABLE `{table}` (value TEXT)')
-
-        self._knownkeys.add(key)
-
-    def llen(self, key) -> int:
-        db = self.router.connection(key)
-        try:
-            l = db.execute(f'SELECT COUNT(*) FROM `{key}-l`').fetchone()
-            r = db.execute(f'SELECT COUNT(*) FROM `{key}-r`').fetchone()
-            return l[0] + r[0]
-        except sqlite3.OperationalError:
-            return 0
-        finally:
-            db.close()
-
-    def rpush(self, key, val) -> int:
-        db = self.router.connection(key)
-        self._ensure_exists(db, key, 'list')
-        try:
-            db.execute(f'INSERT INTO `{key}-r` VALUES(?)', (val,)).fetchone()
-            db.commit()
-        finally:
-            db.close()
-
-    def lpush(self, key, val) -> int:
-        db = self.router.connection(key)
-        self._ensure_exists(db, key, 'list')
-        try:
-            db.execute(f'INSERT INTO `{key}-l` VALUES(?)', (val,)).fetchone()
-            db.commit()
-        finally:
-            db.close()
-
-    def rpop(self, key) -> int:
-        db = self.router.connection(key)
-        self._ensure_exists(db, key, 'list')
-        try:
-            table = f'{key}-r'
-            res = db.execute(
-                f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID DESC LIMIT 1').fetchone()
-            if not res:
-                table = f'{key}-l'
-                res = db.execute(
-                    f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID ASC LIMIT 1').fetchone()
-                if not res:
-                    return None
-            rowid, val = res
-            db.execute(f'DELETE FROM `{table}` WHERE ROWID=?', (rowid,))
-            db.commit()
-            return val
-        except Exception as e:
-            raise
-        finally:
-            db.close()
-
-    def lpop(self, key) -> int:
-        db = self.router.connection(key)
-        self._ensure_exists(db, key, 'list')
-        try:
-            table = f'{key}-l'
-            res = db.execute(
-                f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID DESC LIMIT 1'
-                ).fetchone()
-            if not res:
-                table = f'{key}-r'
-                res = db.execute(
-                    f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID ASC LIMIT 1'
-                ).fetchone()
-                if not res:
-                    return None
-            rowid, val = res
-            db.execute(f'DELETE FROM `{table}` WHERE ROWID=?', (rowid,))
-            db.commit()
-            return val
-        except Exception as e:
-            raise
-        finally:
-            db.close()
+        modules = [lists]
+        for module in modules:
+            for attr in dir(module):
+                if attr.startswith('_'):
+                    continue
+                obj = getattr(module, attr)
+                if not callable(obj):
+                    continue
+                setattr(StrictRedis, attr, obj)
 
 
 if __name__ == '__main__':
---
+++ lists.py
@@ -0,0 +1,102 @@
+'''
+clodss: list data-structure
+'''
+
+
+def _ensure_exists(instance, db, key, dtype):
+    if key in instance._knownkeys:
+        return
+    TABLES = {
+        'list': ['<key>-l', '<key>-r']
+    }
+
+    tables = [tname.replace('<key>', key) for tname in TABLES[dtype]]
+    x = db.execute('SELECT 1 FROM sqlite_master WHERE '
+                   'type="table" AND name=?', (tables[0],)).fetchone()
+    if x is None:
+        for table in tables:
+            db.execute(f'CREATE TABLE `{table}` (value TEXT)')
+
+    instance._knownkeys.add(key)
+
+
+def llen(instance, key):
+    db = instance.router.connection(key)
+    try:
+        l = db.execute(f'SELECT COUNT(*) FROM `{key}-l`').fetchone()
+        r = db.execute(f'SELECT COUNT(*) FROM `{key}-r`').fetchone()
+        return l[0] + r[0]
+    except sqlite3.OperationalError:
+        return 0
+    finally:
+        db.close()
+
+
+def rpush(instance, key, val) -> int:
+    db = instance.router.connection(key)
+    _ensure_exists(instance, db, key, 'list')
+    try:
+        db.execute(f'INSERT INTO `{key}-r` VALUES(?)', (val,)).fetchone()
+        db.commit()
+    finally:
+        db.close()
+
+
+def lpush(instance, key, val) -> int:
+    db = instance.router.connection(key)
+    _ensure_exists(instance, db, key, 'list')
+    try:
+        db.execute(f'INSERT INTO `{key}-l` VALUES(?)', (val,)).fetchone()
+        db.commit()
+    finally:
+        db.close()
+
+
+def rpop(instance, key) -> int:
+    db = instance.router.connection(key)
+    _ensure_exists(instance, db, key, 'list')
+    try:
+        table = f'{key}-r'
+        res = db.execute(
+            f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID DESC LIMIT 1'
+        ).fetchone()
+        if not res:
+            table = f'{key}-l'
+            res = db.execute(
+                f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID ASC LIMIT 1'
+            ).fetchone()
+            if not res:
+                return None
+        rowid, val = res
+        db.execute(f'DELETE FROM `{table}` WHERE ROWID=?', (rowid,))
+        db.commit()
+        return val
+    except Exception as e:
+        raise
+    finally:
+        db.close()
+
+
+def lpop(instance, key) -> int:
+    db = instance.router.connection(key)
+    _ensure_exists(instance, db, key, 'list')
+    try:
+        table = f'{key}-l'
+        res = db.execute(
+            f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID DESC LIMIT 1'
+        ).fetchone()
+        if not res:
+            table = f'{key}-r'
+            res = db.execute(
+                f'SELECT ROWID, value FROM `{table}` ORDER BY ROWID ASC LIMIT 1'
+            ).fetchone()
+            if not res:
+                return None
+        rowid, val = res
+        db.execute(f'DELETE FROM `{table}` WHERE ROWID=?', (rowid,))
+        db.commit()
+        return val
+    except Exception as e:
+        raise
+    finally:
+        db.close()
