86d75c92c9986e03349b63237dd7eb3ef6b5541f
Mohamed Abdel Maksoud <mohamed@amaksoud.com>
2019-12-22 15:00:54 +0100

    moves router to separate module

--- clodss.py
+++ clodss.py
@@ -1,23 +1,5 @@
 import os
-import sqlite3
-import hashlib
-
-
-class Router:
-    def __init__(self, dbpath, factor=3):
-        self.factor = factor
-        self.dbpath = dbpath
-
-    def connection(self, key: bytes):
-        # TODO pooling
-        db = hashlib.sha1(key.encode('utf-8')).hexdigest()[:self.factor]
-        conn = sqlite3.connect(
-            os.path.join(self.dbpath, f'{db}.db'),
-            check_same_thread=False
-        )
-        conn.execute('pragma journal_mode=wal')
-        return conn
-
+from router import Router
 
 class StrictRedis:
     def __init__(self, db: int = 0, sharding_factor: int = 3, base: str = 'data', decode_responses: bool = False) -> None:
