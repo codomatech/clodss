b6526c32d6ec817f05ceb6f9ef67bf266323a7b1
Mohamed Abdel Maksoud <mohamed.amaksoud@gmail.com>
2019-12-28 11:33:12 +0100

    adds docstrings to router

--- clodss/router.py
+++ clodss/router.py
@@ -1,3 +1,8 @@
+'''
+router.py: provides the Router class to map keys to a corresponding database.
+routing is configured by a sharding factor
+'''
+
 import os
 import sqlite3
 import hashlib
@@ -7,6 +12,7 @@ from ilock import ILock
 
 
 class DBConnection:
+    'a database connection used within a pool'
     def __init__(self, fname):
         self.id = uuid.uuid1().hex
         self.free = True
@@ -16,34 +22,43 @@ class DBConnection:
         self._conn = conn
 
     def acquire(self):
+        'acquires this connection'
         self.free = False
 
     def cursor(self):
+        'sqlite3 cursor'
         return self._conn.cursor()
 
     def execute(self, *args, **kwargs):
+        'sqlite3 execute'
         return self._conn.execute(*args, **kwargs)
 
     def executemany(self, *args, **kwargs):
+        'sqlite3 executemany'
         return self._conn.executemany(*args, **kwargs)
 
     def executescript(self, *args, **kwargs):
+        'sqlite3 executescript'
         return self._conn.executescript(*args, **kwargs)
 
     def commit(self):
+        'sqlite3 commit'
         self._conn.commit()
 
     def close(self):
+        'frees this connection'
         self.free = True
 
 
 class Router:
+    'main routing class, maps keys to a db based on a sharding factor'
     def __init__(self, dbpath, factor=3):
         self.factor = factor
         self.dbpath = dbpath
         self.connections = {}
 
     def connection(self, key: bytes):
+        'gets a new connection'
         db = hashlib.sha1(key.encode('utf-8')).hexdigest()[:self.factor]
         fname = os.path.join(self.dbpath, f'{db}.db')
 
