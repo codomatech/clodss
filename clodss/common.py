e0e468c52c875f69a1bd89ef3b7b817c9f2dd7ce
Mohamed Abdel Maksoud <mohamed.amaksoud@gmail.com>
2020-03-13 16:12:46 +0100

    replaces problematic symbols better in keywords

--- clodss/common.py
+++ clodss/common.py
@@ -42,3 +42,28 @@ def _clearexpired(instance, db, key):
         db.commit()
         if key in instance.keystoexpire:
             del instance.keystoexpire[key]
+
+
+class ProblematicSymbols:
+    'remove symbols that could cause sql injection or use reserved words'
+
+    FWD = {
+        'sqlite_': 'sqlite﹁',
+        '`': "﹁'﹁",
+    }
+
+    BWD = {v: k for k, v in FWD.items()}
+
+    @staticmethod
+    def remove(text):
+        'remove symbols'
+        for kw, rep in ProblematicSymbols.FWD.items():
+            text = text.replace(kw, rep)
+        return text
+
+    @staticmethod
+    def restore(text):
+        'resotre symbols'
+        for kw, rep in ProblematicSymbols.BWD.items():
+            text = text.replace(kw, rep)
+        return text
