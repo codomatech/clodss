7e6aa7c29af01529867b1cc1b421f3a6e91c588d
Mohamed <mohamed.amaksoud@gmail.com>
2020-06-17 01:06:56 +0200

    Define dependency & increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clodss",
-    version="0.0.1",
+    version="0.0.2",
     author="Codoma.tech Team",
     author_email="info@codoma.tech",
     description="On-Disk data-structures store with redis-like API.",
@@ -19,4 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'ilock',
+    ],
 )
