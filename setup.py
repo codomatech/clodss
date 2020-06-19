894cb44402f78d629519336da1ad9d66f286d42f
Mohamed <mohamed.amaksoud@gmail.com>
2020-06-19 13:55:25 +0200

    Update README and Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clodss",
-    version="0.0.2",
+    version="0.0.3",
     author="Codoma.tech Team",
     author_email="info@codoma.tech",
     description="On-Disk data-structures store with redis-like API.",
