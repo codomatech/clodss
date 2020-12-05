b2524a2e05c0b2daccd55021526bd7e942498c06
Mohamed Abdel Maksoud <mohamed.amaksoud@gmail.com>
2020-12-05 23:50:22 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clodss",
-    version="0.0.4",
+    version="0.0.5",
     author="Codoma.tech Team",
     author_email="info@codoma.tech",
     description="On-Disk data-structures store with redis-like API.",
